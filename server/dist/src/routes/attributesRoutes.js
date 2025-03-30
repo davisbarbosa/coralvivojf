64d6c5665dd86797dee740015fa847f3777013d7
davis.barbosa <davis.barbosa@levty.com>
2025-03-30 10:43:25 -0300

    teste

--- server/dist/src/routes/attributesRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const attributesControllers_1 = require("../controllers/attributesControllers");
-const router = (0, express_1.Router)();
-router.get("/category/:categoryId", attributesControllers_1.getAttributesByCategory);
-router.get("/:attributeId/values", attributesControllers_1.getValuesByAttribute);
-exports.default = router;
